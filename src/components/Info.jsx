22110b482892211e23f3f7e79c3783fde9674142
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-08 14:46:34 +0100

    Info Component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,3 +1,11 @@
 export default function Info() {
-    
+    return (
+        <header>
+            <img src="/Barnyard_Biggie_Cheese.webp" />
+            <h1>Biggie Cheese</h1>
+            <p className="job-title">Music Figure</p>
+            <p className="username">big.E.cheese</p>
+            <button><i class="fa-solid fa-envelope"></i>Email</button>
+        </header>
+    );
 }
