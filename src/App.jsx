cff3467dfff90f18e4a14b24af14f0aa9f1c37e3
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-08 14:31:55 +0100

    Component set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Info from "./components/Info";
+import Interests from "./components/Interests";
 
 function App() {
-
+  return (
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
