8b887bd05e82cf839f0bbd371347d8c2f4bc243e
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-08 14:52:10 +0100

    About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,13 @@
 export default function About() {
-    
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>
+                Smooth operator. Mic dropper. Wheel of steel connoisseur.
+                Spreading cheddar and good vibes since day one.
+                <br></br>
+                🎤 Performer • 🎵 Music Maven • 🧀 Cheese Aficionado
+            </p>
+        </section>
+    );
 }
